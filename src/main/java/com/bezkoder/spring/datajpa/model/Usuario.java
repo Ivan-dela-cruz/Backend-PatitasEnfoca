7093c13e9c01c31572b2ce1a4b220a7fbaece50a
ANDRES PIEDRA <pandreslevelap@gmail.com>
2020-12-30 10:53:48 -0500

    usuario

---
+++ src/main/java/com/bezkoder/spring/datajpa/model/Usuario.java
@@ -0,0 +1,91 @@
+package com.bezkoder.spring.datajpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "nombres")
+	private String nombres;
+	
+	@Column(name = "apellidos")
+	private String apellidos;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "usuario")
+	private String usuario;
+	
+	
+	public Usuario(String nombres, String apellidos, String email, String password, String usuario) {
+		super();
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.password = password;
+		this.usuario = usuario;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+}
